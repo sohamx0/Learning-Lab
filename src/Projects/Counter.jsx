10bf3aa497781b4ff8b0870e8c03cf858765427f
Soham <sohamx0@users.noreply.github.com>
2026-06-13 22:13:32 +0530

    feat : added increament , decreament and rest logic with css styling

--- src/Projects/Counter.jsx
+++ src/Projects/Counter.jsx
@@ -1,17 +1,33 @@
 import { useState } from 'react'
-
+import './Counter.css'
 
 export default function Counter() {
   const [count, setCount] = useState(0)
-  function clicked() {
+  function increament() {
     setCount(count + 1)
+  }
+  function decreament() {
+    setCount(count - 1)
+  }
+  function reset() {
+    setCount(0)
   }
   return (
     <>
-    <p>{count}</p>
-    <button onClick={clicked}>
-      Click me
-    </button>
-    </>
+      <div className="container">
+        <p>{count}</p>
+        <div className='btn-container'>
+          <button className="btn" onClick={increament}>
+            increament
+          </button>
+          <button className="btn" onClick={decreament}>
+            decreament
+          </button>
+          <button className="btn" onClick={reset}>
+            reset
+          </button>
+        </div>
+      </div>
+      </>
   );
 }
